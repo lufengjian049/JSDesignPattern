23319c4616b9bc080328bfb8efada7bf50b723fe
lufj <lufengjian0409@163.com>
2017-09-05 16:50:06 +0800

    ++ algorithm

--- Communicate/ques-algorithm.js
+++ Communicate/ques-algorithm.js
@@ -76,6 +76,97 @@ const arrangeCategories = (categories, parent = null) => {
    return result
  }
 
- //桶排序
- //冒泡排序
- //快速排序
+ function sort(sarr){
+    //常规方法
+    for(var a=[],n=0;n<100;a[n++] = 0);
+    // Array.apply(null,Array(100)).map(_ => 0);
+    // Array(101).join(0).split("").map(n=>+n);
+    // //es6
+    // Array.from({length:100}, _ => 0);
+    // [...Array(100)].map(_ => 0);
+    // [].fill.call(Array(100),0); //Array(100).fill(0)
+
+    sarr.forEach(function(sitem){
+      a[sitem]++;
+    })
+    for(var ret=[],i=0;i<100;i++){
+      // if(a[i] > 0)
+      //   ret[ret.length] = i;
+      var count = a[i];
+      while(count--){
+        ret[ret.length] = i;
+      }
+    }
+    return ret;
+ }
+ const slist = [54, 26, 93, 17, 77, 31, 44, 55, 20,26];
+ console.log("sort");
+ console.log(sort(slist));
+
+ function bubbleSort(sarr){
+   for(var i=0,len=sarr.length;i<len;i++){
+     for(var j=0;j<len-i-1;j++){
+       if(sarr[j] > sarr[j+1]){
+         var tmp = sarr[j];
+         sarr[j] = sarr[j+1];
+         sarr[j+1] = tmp;
+       }
+     }
+   }
+   return sarr;
+ }
+ console.log("bsort");
+ console.log(bubbleSort([54, 26, 93, 17, 77, 31, 44, 55, 20,26]));
+
+//  function quickSort(sarr){
+//   if(sarr.length == 0)
+//       return [];
+//   var i = 0;
+//   var j = sarr.length - 1;
+//   var base = sarr[i];//基准值在左，右边先动找< 基准的值
+//   while(i<j){
+//     while(sarr[j] >= base && i < j){
+//       j--;
+//     }
+//     while(sarr[i] <= base && i < j){
+//       i++;
+//     }
+//     var tmp = sarr[i];
+//     sarr[i] = sarr[j];
+//     sarr[j] = tmp;
+//   }
+//   sarr[0] = sarr[j];
+//   sarr[j] = base;
+//   return [].concat(quickSort(sarr.slice(0,j)),sarr[j],quickSort(sarr.slice(j+1)));
+//   // quickSort(sarr,0,j-1);
+//   // quickSort(sarr,j+1,right);
+//   // return sarr;
+//  }
+function quickSort(sarr,left,right){
+  if(left > right){
+      return;
+  }
+  left = left || 0;
+  right = right || (sarr.length - 1);
+  var i = left;
+  var j = right;
+  var base = sarr[left];
+  while(i<j){
+      while(sarr[j] >= base && i < j){
+          j--;
+      }
+      while(sarr[i] <= base && i < j){
+          i++;
+      }
+      var tmp = sarr[j];
+      sarr[j] = sarr[i];
+      sarr[i] = tmp;
+  }
+  sarr[left] = sarr[j];
+  sarr[j] = base;
+  arguments.callee(sarr,left,j - 1);
+  arguments.callee(sarr,j+1,right);
+  return sarr;
+}
+ console.log("quickSort");
+ console.log(quickSort([54, 26, 93, 17, 77, 31, 44, 55, 20,26,88,3,89]));
--- Communicate/ques-thinking.js
+++ Communicate/ques-thinking.js
@@ -49,6 +49,8 @@ matchesSelector(
 // HTML元素的属性名是大小写不敏感的，但是JavaScript对象的属性名是大小写敏感的。转换规则是，转为JavaScript属性名时，一律采用小写。
 // 如果属性名包括多个单词，则采用骆驼拼写法，即从第二个单词开始，每个单词的首字母采用大写
 
+//浏览器总是假定click事件的目标节点，就是点击位置的嵌套最深的那个节点
+
 //https://nx-framework.com/docs/basics/structure
 //http://www.zcfy.cc/article/writing-a-javascript-framework-project-structuring-risingstack-1675.html
 
