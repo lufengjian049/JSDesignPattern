ce40ab98aae3a2eb9127d373a9c7930116177745
lufj <lufengjian0409@163.com>
2017-09-22 18:04:01 +0800

    ++

--- Communicate/ques-test.js
+++ Communicate/ques-test.js
@@ -69,3 +69,28 @@ function _diffence(){
 }
 console.log(_diffence([1, 2, 3, 4, 5], [5, 2, 10], [4], 3));
 //shallow=false,strict=true; => []
+
+//unique
+function unique(arr){
+  var map = new Map(),retarr=[];
+  for(var i=0;i<arr.length;i++){
+    var value = arr[i];
+    if(!map.get(value)){
+      map.set(value,true);
+      retarr.push(value);
+    }
+  }
+  return retarr;
+}
+function unique2(arr){
+  return [...new Set(arr)];
+}
+function unique3(arr){
+  return arr.filter(function(item,index){
+    return arr.indexOf(item) == index;
+  })
+}
+var testunique = [1,'2',undefined,undefined,'2',NaN,NaN,null,null,{},{},/a/,/a/,[],[]];
+console.log(unique(testunique));
+console.log(unique2(testunique));
+console.log(unique3(testunique));
