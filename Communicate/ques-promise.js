57f5e1a746c9fb820374dc489551affb63a20c74
lufj <lufengjian0409@163.com>
2018-05-04 18:19:30 +0800

    promise

--- Communicate/ques-promise.js
+++ Communicate/ques-promise.js
@@ -103,12 +103,17 @@ function getUserJobInfo(id) {
 //immediate -- https://github.com/calvinmetcalf/immediate/blob/master/lib/index.js
 //https://zhuanlan.zhihu.com/p/34421918
 //https://github.com/whinc/blog/issues/2
+//bluebird https://www.ibm.com/developerworks/cn/web/wa-lo-use-bluebird-implements-power-promise/index.html
 //0.为什么then方法是异步调用? 同步和异步可能会引发混乱
 //1.为什么.then方法返回的是一个新的promise对象而不是this，而进行链式调用?
 //2.为什么.then的执行函数只会执行一次？
       // promise对象的状态从pending转换为Fulfilled或者Rejected后，这个状态就不可变了(settled);所以then的回调只执行一次
 //3.Promise.resolve()方法的功效? 1.new Promise的快捷方式 2.将thenable对象转化为Promise对象
 //4.什么是thenable对象？ 具有.then方法的一类对象，但是具有与Promise所拥有的then方法相同的功能和处理过程
+//then函数必须有返回值；
+//then函数返回的是一个非promise对象，需要resolve完成then返回的promise对象
+//                  promise对象，给promise对象附加then方法，在附加的then方法内，resolve完成父promise(then返回的promise)的状态。
+//                  附加的then方法，其实只做一件事件，就是resolve操作。跟new Promise(exector)的exector一样
 function PromiseNew(executor) {
   var self = this;
   this.status = 'pending';
@@ -222,14 +227,14 @@ var taskPromise = new Promise(resolve => {
     resolve(delay + 'ms');
   },delay);
 })
-timeoutPromise(taskPromise,1000).then(value => {
-  console.log('end:' + value);
-}).catch(error => {
-  if(error instanceof TimeoutError) {
-    //取消 接口请求的abort
-  }
-  console.log('error:' + (error instanceof TimeoutError));
-})
+// timeoutPromise(taskPromise,1000).then(value => {
+//   console.log('end:' + value);
+// }).catch(error => {
+//   if(error instanceof TimeoutError) {
+//     //取消 接口请求的abort
+//   }
+//   console.log('error:' + (error instanceof TimeoutError));
+// })
 
 //给一个函数附加了太多的功能，根据分离职责的原则，可以将功能分离出去
 function createXHRPromise1(url) {
@@ -271,3 +276,20 @@ function cancelPromise(promise) {
     request.abort();
   }
 }
+
+async function asyncfn() {
+  const p1 = new Promise(resolve => setTimeout(() => {resolve(111);console.log('p1')},2000));
+  const p2 = new Promise(resolve => setTimeout(() => {resolve(222);console.log('p2')},1000));
+  const response1 = await p1;
+  console.log('response 1',response1);
+  const response2 = await p2;
+  console.log('response 2',response2);
+  // return response;
+  return {
+    response1,
+    response2
+  }
+}
+asyncfn().then(value => console.log('from async',value));
+
+//callback -> promise -> generator(co) -> async/await
